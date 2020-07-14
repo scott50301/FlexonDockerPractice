661c4571d68c55e08c76fc9021a25a1a55d1c408
scott50301@gmail.com <scott50301@gmail.com>
2020-07-13 22:29:12 -0700

    use spring boot

---
+++ src/main/java/com/Scott/Classes/Customers.java
@@ -0,0 +1,54 @@
+package com.Scott.Classes;
+
+import java.math.BigDecimal;
+
+public class Customers {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private BigDecimal phone;
+    private String email;
+
+    public Customers(int id, String firstName, String lastName, BigDecimal phone, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public void setFirstNema(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getFirstNema(){
+        return firstName;
+    }
+
+    public void setLastNema(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getLastNema(){
+        return lastName;
+    }
+
+    public void setPhone(BigDecimal phone){
+        this.phone = phone;
+    }
+
+    public BigDecimal getPhone(){
+        return phone;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+}
