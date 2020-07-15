8b709415a66ab9f094f5a66082023297ddee2f9c
scott50301 <scott50301@gmail.com>
2020-07-15 14:18:54 -0700

    delete unused library

--- src/main/java/com/Scott/DAO/CustomersDAO.java
+++ src/main/java/com/Scott/DAO/CustomersDAO.java
@@ -1,21 +1,9 @@
 package com.Scott.DAO;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
-import java.sql.*;
 import com.Scott.Classes.Customers;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 
 @Repository
 public interface CustomersDAO extends CrudRepository<Customers, Long> {
